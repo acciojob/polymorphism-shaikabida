c414d6392a83685f757d78f0a8a3280b9bd16471
shaikabida <73274964+shaikabida@users.noreply.github.com>
2022-10-05 11:46:06 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,28 @@
 package com.driver;
 
-public class Main {
-
-    
-  }
+public class Main
+{
+	public static void main(String[] args) {
+		Product p=new Product();
+		int method1=p.product(1,2);
+		int method2=p.product(3,4,5);
+		double method3=p.product(6,7);
+		System.out.print(method1+" "+method2+" "+method3);
+	}
+}
+class Product
+{
+   public int product(int x, int y) {
+       int prod=x*y;
+       return prod;
+   } 
+   public int product(int x, int y, int z) {
+       int prod=x*y*z;
+       return prod;
+   }
+   public double product(double x, double y) {
+       double prod=x*y;
+       return prod;
+   }
+   
+}
